26dd7abcc546d21a3d020dd621877d40db5d0796
Grottersha123 <grottersha123@gmail.com>
2015-07-06 19:19:48 +0300

    Добавлены новые комментарии к коду

--- More_little/More_little.ino
+++ More_little/More_little.ino
@@ -1,6 +1,6 @@
 int val;
-int ledRed = 3;
-int ledGrean = 6;
+int ledRed = 3; // 3 пин
+int ledGrean = 6; // 6 пин
 
 void setup()
 { 
@@ -9,13 +9,13 @@ void setup()
 void loop()
 {
   for (val = 0; val < 255; val++){ 
-    analogWrite(ledGrean,val);
+    analogWrite(ledGrean,val); //увеличивается до 255
     analogWrite(ledRed,255-val); // изменяется значение от 255 до 0
     delay(5);
   }
   delay(1000);
   for (val = 255; val !=0; val--){
-    analogWrite(ledGrean, val);
+    analogWrite(ledGrean, val); //уменьшается до 0
     analogWrite(ledRed,255-val); от 0 до 255 
     delay(5);
   }
