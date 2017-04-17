968467babe85daebe68505571b4030ffdf971f91
Grottersha123 <grottersha123@gmail.com>
2017-04-18 00:17:39 +0300

    добавление сразу в две таблицы

--- SAP_ARDUINO_PYTHON/SAP_ARDUINO_PYTHON.ino
+++ SAP_ARDUINO_PYTHON/SAP_ARDUINO_PYTHON.ino
@@ -48,7 +48,7 @@ void setup() {
   Serial.begin(9600); // connect serial
   gpsSerial.begin(9600); // connect gps sensor
 
-  makeCoords(N1, E1, N2, E2, 10);
+ // makeCoords(N1, E1, N2, E2, 10);
   
 }
 float lat, lon;
@@ -56,11 +56,12 @@ unsigned long fix_age;
 void loop() {
 
   //GET_GPS();
-
+  PYTHON_SEND_TWO_TABLE(100,100,1,23,34,45);
+  
 
 
 }
-
+// берет данные с модуля и перекодирует в нужный формат
 void GET_GPS()
 { 
   while (gpsSerial.available()) { // check for gps data
@@ -104,86 +105,29 @@ void PYTHON_SEND(float lan, float lon)
   p.close();
 }
 
-
-
-
-
-void SEND_DATA(float Lot, float Lat)
+void PYTHON_SEND_TWO_TABLE(float lan, float lon,int id,float hum,float light, float temp)
 {
-  Process p;
-
+  String cmd;
+  Process p; // Create a process and call it "p"
+  
+  p.begin("python"); // Process that launch the "python" command
   String str1;
   char tmp1[10];
   char tmp2[10];
-  dtostrf(Lot, 1, 6, tmp1);
+  dtostrf(lan, 1, 6, tmp1);
   String strLab1 = "";
   String strOut1 = strLab1 + tmp1;
   //
-  dtostrf(Lat, 1, 6, tmp2);
+  dtostrf(lon, 1, 6, tmp2);
   String strLab = "";
   String strOut = strLab + tmp2;
-
-  str1 = "{\"mode\":\"async\",\"messageType\":\"" + MESSAGE + "\",\"messages\":[{\"timestamp\":" + String(0) + ",\"Latitude\":" + String(strOut1) + ",\"Longitude\":" + String(strOut) + "}]}";
-
-  //char cmd[] = "curl -i -k -f -H Authorization: Bearer "+ TOKEN + " -H Content-Type: application/json;charset=UTF-8 -X POST https://iotmmsp" + NAME_ACCOUNT + ".hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/9d791d17-f73f-47d7-941b-69ea6c670939 -d";
-  //char cmd1[] = "curl -i -k -f -H Authorization: Bearer " + TOKEN + " -H Content-Type: application/json;charset=UTF-8 -X POST https://iotmmsp" + NAME_ACCOUNT + ".hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/";
-  char hellostr[] =  "<html><head><title>hello world</title></head>";
-
- // Serial.println(cmd.length());
- // Serial.println(cmd1.length());
- // p.runShellCommand(cmd + str1);
- // Serial.println(cmd1);
-  // Serial.println(p.exitValue());
-
-  //p.close();
-
-}
-
-void pushToCloud1(float Lot, float Lat)
-{
-  // curl --header 'Authorization: Bearer 5adce4964138342473a663b2d22e8' --header 'Content-Type:application/json;charset=UTF-8' -k -X POST https://iotmmsp1941889917trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/d70fbdef-dba4-4f59-85eb-9e8131582b3c --data '{"mode":"async","messageType":"091b562d43f936bef85b","messages":[{"Temperature":23, "Light":210, "Humidity":65, "ID":1}]}'
-  Process p;
-  p.begin("curl");
-
-  p.addParameter("-k");
-  p.addParameter("-f");
-  // p.addParameter("-i");
-  // p.addParameter("-v");
-  p.addParameter("-H");
-  p.addParameter("Authorization: Bearer " + TOKEN);
-  p.addParameter("-H");
-  p.addParameter("Content-Type: application/json;charset=UTF-8");
-  p.addParameter("-X");
-  p.addParameter("POST");
-  //p.addParameter("https://iotmmsp1942369040trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/c2721cd2-3e96-4858-9cb6-7fb5d0c7a29e");//TLH
-  //p.addParameter("https://iotmmsp" + NAME_ACCOUNT + ".hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/9d791d17-f73f-47d7-941b-69ea6c670939"); //geo
-  p.addParameter("http://requestb.in/1ahfbrj1"); //geo
-  p.addParameter("--data");
-  String str1;
-  char tmp1[10];
-  char tmp2[10];
-  dtostrf(Lot, 1, 6, tmp1);
-  String strLab1 = "";
-  String strOut1 = strLab1 + tmp1;
-  //
-  dtostrf(Lat, 1, 6, tmp2);
-  String strLab = "";
-  String strOut = strLab + tmp2;
-
-  str1 = "{\"mode\":\"async\",\"messageType\":\"" + MESSAGE + "\",\"messages\":[{\"timestamp\":" + String(0) + ",\"Latitude\":" + String(strOut1) + ",\"Longitude\":" + String(strOut) + "}]}";
-  p.addParameter(str1);
-  p.run();
-  /*while (p.available() > 0) {
-    char c = p.read();
-    Serial.print(c);
-  }*/
-  Serial.println(str1);
+  cmd = "python /mnt/sda1/arduino/r.py "+String(strOut1) + " " + String(strOut) + " " + String(id)+ " " + String(hum)+ " " + String(light)+ " " + String(temp);
+  p.runShellCommand(cmd);
   Serial.print("Exit: ");
-
   Serial.println(p.exitValue());
   p.close();
-  //Serial.println(p.read());
 }
+
 
 
 
--- SAP_ARDUINO_PYTHON/r.py
+++ SAP_ARDUINO_PYTHON/r.py
@@ -3,31 +3,31 @@ import time
 import json
 import sys
 import urllib2
+import TOKENS
 first, second = 55.826090, 37.387760
 third,fourth = 55.825867,  37.389916
 n  = 10
-#import urllib2
 #TOKEN1 = "72aa5381427f62e323ac3a7be704e7d"
 
-TOKEN = "115666ccda8e12ffcd176f9a2c14b72"
+"""TOKEN = "115666ccda8e12ffcd176f9a2c14b72"
 NAME_ACCOUNT = "1942369040trial"
 MESSAGE = "289ffc0c7adba1446c17"
 
-MESSAGE_DEVICE = "9d791d17-f73f-47d7-941b-69ea6c670939"
+MESSAGE_DEVICE = "9d791d17-f73f-47d7-941b-69ea6c670939"""
 
 
 def send_data_urllib(lat, lon):
     headers = {
-        "Authorization": "Bearer {token}".format(token=TOKEN),
+        "Authorization": "Bearer {token}".format(token=TOKENS.TOKEN),
         "Content-Type": "application/json;charset=UTF-8",
     }
 
     url = "https://iotmmsp{account}.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/{device}". \
-        format(account=NAME_ACCOUNT,device = MESSAGE_DEVICE)
+        format(account=TOKENS.NAME_ACCOUNT,device = TOKENS.MESSAGE_DEVICE)
 
     data = {
         "mode": "async",
-        "messageType": MESSAGE,
+        "messageType": TOKENS.MESSAGE,
         "messages": [
             {
                 "timestamp": int(time.time()),
@@ -40,7 +40,55 @@ def send_data_urllib(lat, lon):
     req = urllib2.Request(url, json.dumps(data), headers)
     response = urllib2.urlopen(req)
     return response.code
+def send_data_urllib_T(lat, lon):
+    headers = {
+        "Authorization": "Bearer {token}".format(token=TOKENS.TOKEN),
+        "Content-Type": "application/json;charset=UTF-8",
+    }
 
+    url = "https://iotmmsp{account}.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/{device}". \
+        format(account=TOKENS.NAME_ACCOUNT,device = TOKENS.MESSAGE_DEVICE)
+
+    data = {
+        "mode": "async",
+        "messageType": TOKENS.MESSAGE_1,
+        "messages": [
+            {
+                "timestamp": int(time.time()),
+                "Lat": round(float(lat), 6),
+                "Lon": round(float(lon), 6),
+            }
+        ]
+    }
+
+    req = urllib2.Request(url, json.dumps(data), headers)
+    response = urllib2.urlopen(req)
+    return response.code
+def send_data_urllib_A(id,hum,light,temp):
+    headers = {
+        "Authorization": "Bearer {token}".format(token=TOKENS.TOKEN),
+        "Content-Type": "application/json;charset=UTF-8",
+    }
+
+    url = "https://iotmmsp{account}.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/{device}". \
+        format(account=TOKENS.NAME_ACCOUNT,device = TOKENS.MESSAGE_DEVICE)
+
+    data = {
+        "mode": "async",
+        "messageType": TOKENS.MESSAGE_2,
+        "messages": [
+            {
+                "ID": int(id),
+                "Humanity": round(float(hum),3 ),
+                "Light": round(float(light), 3),
+                "Temperature" : round(float(temp),3)
+            }
+        ]
+    }
+
+    req = urllib2.Request(url, json.dumps(data), headers)
+    response = urllib2.urlopen(req)
+    return response.code
 def coordinates(first, second, third, fourth, n):
     del1 = (third - first) / n
     del2 = (fourth - second) / n
@@ -50,10 +98,16 @@ def coordinates(first, second, third, fourth, n):
         status = send_data_urllib(first ,second)
         print(status)
 if __name__ == "__main__":
-    #lat = sys.argv[-2]
-    #lon = sys.argv[-1]
-    # status = send_data(lat, lon)
-    ##a = {55.826068:37.387976,55.826045:37.388191,55.826023:37.388407,55.826001:37.388622,55.825979:37.388838,55.825956:37.389054,55.825934:37.389269,55.825912:37.389485,55.825889:37.3897,55.825867:37.389916}
-    coordinates(first, second, third, fourth, n)
+    lat = sys.argv[-6]
+    lon = sys.argv[-5]
+    id = sys.argv[-4]
+    hum = sys.argv[-3]
+    light = sys.argv[-2]
+    temp = sys.argv[-1]
 
-    #print(status)
+    status_g = send_data_urllib_T(lat, lon)
+    status_s = send_data_urllib_A(id,hum,light,temp)
+    ##a = {55.826068:37.387976,55.826045:37.388191,55.826023:37.388407,55.826001:37.388622,55.825979:37.388838,55.825956:37.389054,55.825934:37.389269,55.825912:37.389485,55.825889:37.3897,55.825867:37.389916}
+    #coordinates(first, second, third, fourth, n)
+
+    print(status_g,status_s)
